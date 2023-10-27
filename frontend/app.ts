16449951d8b277edadcc07b87d6ad782f47696cf
gongahkia <angryapplegravy@gmail.com>
2023-10-27 09:53:20 +0800

    default message

---
+++ frontend/app.ts
@@ -0,0 +1,3 @@
+// implement logic for python interpreter in typescript and integrate app.js with a front-end html rendered in typescript 
+
+// intention is to render a live display of the draft charge using html
